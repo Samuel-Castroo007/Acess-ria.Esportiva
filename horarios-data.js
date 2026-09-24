b6b34e42878fb927339d133ae24a5846b999e6f1
Samuel Castro Personal <samuelcastropersonal2009@gmail.com>
2026-09-24 14:52:50 -0300

    Update GitHub owner and repository names

--- horarios-data.js
+++ horarios-data.js
@@ -20,8 +20,8 @@
    ===================================================================== */
 (function (global) {
 
-  const GITHUB_OWNER  = 'SEU-USUARIO';       // seu usuário do GitHub
-  const GITHUB_REPO   = 'SEU-REPOSITORIO';   // nome do repositório no GitHub
+  const GITHUB_OWNER  = 'Samuel-Castroo007';       // seu usuário do GitHub
+  const GITHUB_REPO   = 'Acess-ria.Esportiva';   // nome do repositório no GitHub
   const GITHUB_BRANCH = 'main';              // branch onde o site é publicado
   const STATUS_FILE   = 'horarios-status.json';
 
